a702dbb48027ee90e7de4a1407fb20abf11ced93
ArturoSbr <soberonarturo@gmail.com>
2022-05-03 07:34:45 -0500

    Start Matching Question in R

---
+++ cod/matching.R
@@ -0,0 +1,10 @@
+# Librerías
+library(dplyr)
+library(MatchIt)
+
+# Leer datos
+d1 <- read.csv('../dat/baseline.csv')
+d2 <- read.csv('../dat/endline.csv')
+d3 <- read.csv('../dat/completa.csv')
+
+# 
